a58a34415ae4758a817a2789e963b2423900a2e4
mayurisakpal <mayusakpal910@gmail.com>
2020-08-03 12:04:29 +0530

    uibuild

---
+++ build/precache-manifest.0e528d1a95921fe73d80fb0259bdfc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16dcf884847907979b8cf09c0f69a7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a24de487b58387df1ca",
+    "url": "/static/css/main.27e3d046.chunk.css"
+  },
+  {
+    "revision": "3e5a37805f3e80088066",
+    "url": "/static/js/2.42c3a0de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42c3a0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a24de487b58387df1ca",
+    "url": "/static/js/main.6213bdc9.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
